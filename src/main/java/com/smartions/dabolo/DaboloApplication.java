e5c9c4cd8f6825e650bb00695292f6d9f69d33b7
gmfenglin <feng.lin@smartions.com>
2018-04-27 10:44:59 +0800

    去掉代码时间格式设置

--- src/main/java/com/smartions/dabolo/DaboloApplication.java
+++ src/main/java/com/smartions/dabolo/DaboloApplication.java
@@ -23,22 +23,8 @@ import com.smartions.dabolo.model.UploadFile;
 public class DaboloApplication {
 	@Autowired
 	private UploadFile uploadFile;
-	@Bean
-    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
-    	MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-    	//设置日期格式
-    	ObjectMapper objectMapper = new ObjectMapper();
-    	SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-    	objectMapper.setDateFormat(smt);
-    	mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
-    	//设置中文编码格式
-    	List<MediaType> list = new ArrayList<MediaType>();
-    	list.add(MediaType.APPLICATION_JSON_UTF8);
-    	mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
-    	return mappingJackson2HttpMessageConverter;
-    }
-	@Bean
 
+	@Bean
 	public MultipartConfigElement multipartConfigElement() {
 
 		MultipartConfigFactory factory = new MultipartConfigFactory();
